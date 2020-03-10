863eb0a841d5dfe6251ed6263dac0993dde35872
James Allen <jax5121@hotmail.com>
2020-03-10 22:28:32 +0000

    Program updates wallpaper
    Updates wallpaper and does a check if it's already got a file for today before it tries to download

--- FaithLifeWallpaper/Program.cs
+++ FaithLifeWallpaper/Program.cs
@@ -3,15 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Win32;
 
 namespace FaithLifeWallpaper
 {
     class Program
     {
+        const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+        static Screen[] screens;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+        public enum Style : int
+        {
+            Tiled,
+            Centered,
+            Stretched
+        }
+
+        public static void Set(string uri, string path, Style style)
+        {
+            System.IO.Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
+
+            System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+            
+            img.Save(path, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+            if (style == Style.Stretched)
+            {
+                key.SetValue(@"WallpaperStyle", 2.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Centered)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 0.ToString());
+            }
+
+            if (style == Style.Tiled)
+            {
+                key.SetValue(@"WallpaperStyle", 1.ToString());
+                key.SetValue(@"TileWallpaper", 1.ToString());
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER,
+                0,
+                path,
+                SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+
         static void Main(string[] args)
         {
-            System.Windows.Forms.Screen[] screens = System.Windows.Forms.Screen.AllScreens;
-            long l = 2;
+            screens = Screen.AllScreens;
+            string uri = String.Format("https://bible.faithlife.com/verseoftheday/image/{0}", DateTime.Now.ToString("yyyy-MM-dd"));
+            string path = String.Format("E:\\FaithLife\\{0}.jpg", DateTime.Now.ToString("dd-MM-yyyy"));
+            if(!File.Exists(path)) 
+                Set(uri, path, Style.Tiled);
         }
     }
 }
