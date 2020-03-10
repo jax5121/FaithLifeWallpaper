6472ae365e643253f964ca0a2e0672725142fabb
James Allen <jax5121@hotmail.com>
2020-03-10 14:01:28 +0000

    Project creation
    Added forms reference to project ready for screen customisation

---
+++ FaithLifeWallpaper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaithLifeWallpaper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Windows.Forms.Screen[] screens = System.Windows.Forms.Screen.AllScreens;
+            long l = 2;
+        }
+    }
+}
